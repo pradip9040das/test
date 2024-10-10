8ced2d7c1c4a65af4a8c9ba93687da66aafcb656
pradip9040das <144430972+pradip9040das@users.noreply.github.com>
2024-10-10 16:42:35 +0530

    Add files via upload

---
+++ clear_log.py
@@ -0,0 +1,32 @@
+import os
+import yaml
+import datetime
+
+# Function to read YAML file and get the extension
+def get_extension_from_yaml(yaml_file):
+    with open(yaml_file, 'r') as file:
+        config = yaml.safe_load(file)
+        return config.get('log_extension')
+
+# Function to find files with the specified extension
+def find_files_with_extension(extension, directory='.'):
+    log_files = [f for f in os.listdir(directory) if f.endswith(extension)]
+    return log_files
+
+def clear_log(file_path):
+    # Open the file in write mode to clear it
+    with open(file_path, 'w') as file:
+        pass  # No content is written, so the file is cleared
+
+today = datetime.date.today().strftime('%d-%m')
+
+
+# Example usage
+yaml_file = 'actions.yml'
+log_extension = get_extension_from_yaml(yaml_file)
+
+# Search for log files in the current directory
+log_files = find_files_with_extension(log_extension)
+
+if ((log_files) & ((today=='01-01') | (today=='10-10'))):
+    clear_log(log_files)
